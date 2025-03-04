fc29482ea9e2451f34775b9d6c2515cfdf8766a6
TejasAnil4 <tejasanil6@gmail.com>
2025-03-04 19:59:51 +0530

    Data validation done

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-
-
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig
 import sys
 
 if __name__ == '__main__':
@@ -20,10 +19,21 @@ if __name__ == '__main__':
         # Step 4: Initiate data ingestion
         logging.info("Initiate Data Ingestion")
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+        logging.info("Data Ingestion Completed")
         print(data_ingestion_artifact)
+
+        # Step 5: Initialize DataValidation
+        data_validation_config = DataValidationConfig(training_pipeline_config)
+        data_validation = DataValidation(data_ingestion_artifact, data_validation_config)
+
+        # Step 6: Initiate data validation
+        logging.info("Initiate data validation")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info("Data validation completed")
+        print(data_validation_artifact)
 
     except Exception as e:
         raise NetworkSecurityException(
             error_message="Error occurred in main.py",
-            error_details=str(e)
+            error_details=e  # Pass the exception object
         )
--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -1,14 +1,33 @@
 import sys
 from networksecurity.logging import logger
 
-class NetworkSecurityException(Exception):
-    def __init__(self,error_message,error_details:sys):
-        self.error_message = error_message
-        _,_,exc_tb = error_details.exc_info()
+# class NetworkSecurityException(Exception):
+#     def __init__(self,error_message,error_details:sys):
+#         self.error_message = error_message
+#         _,_,exc_tb = error_details.exc_info()
         
-        self.lineno=exc_tb.tb_lineno
-        self.file_name=exc_tb.tb_frame.f_code.co_filename 
+#         self.lineno=exc_tb.tb_lineno
+#         self.file_name=exc_tb.tb_frame.f_code.co_filename 
     
+#     def __str__(self):
+#         return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+#         self.file_name, self.lineno, str(self.error_message))
+    
+class NetworkSecurityException(Exception):
+    def __init__(self, error_message, error_details):
+        """
+        Custom exception class for Network Security errors.
+
+        Args:
+            error_message (str): A descriptive message about the error.
+            error_details (Exception or str): The exception object or error details.
+        """
+        self.error_message = error_message
+        self.error_details = error_details
+        super().__init__(self.error_message)
+
     def __str__(self):
-        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
-        self.file_name, self.lineno, str(self.error_message))
+        """
+        Format the error message for display.
+        """
+        return f"{self.error_message}: {self.error_details}"
